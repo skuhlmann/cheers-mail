0232d713914bfbc826fef779a3b69bd9d3568865
Sam Kuhlmann <samkuhlmann@gmail.com>
2015-04-29 10:22:45 -0600

    Adds buttons and js to reveal sample text and subscription form

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+  $("#subscribe").click(function(){
+   $("#subscribe-form").toggleClass('hidden');
+  });
+
+  $("#sample").click(function(){
+   $("#sample-text").toggleClass('hidden');
+  })
+
+});
